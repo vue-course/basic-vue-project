e6b8244e3e89858d0d3eddaa6792a9df9e78b31f
David Meir-Levy <davidmeirlevy@gmail.com>
2018-07-10 19:53:41 +0300

    indent fix

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import './sass/main.scss';
 
 // tslint:disable-next-line:no-unused-expression
 new Vue({
-  el: '#app-main',
-  data: {
-    instance: 'Using The Vue Instance'
-  }
+    el: '#app-main',
+    data: {
+        instance: 'Using The Vue Instance'
+    }
 });
