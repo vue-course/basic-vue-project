0770a8cdd1e1ea7df33fdfd00c69a505e20030e3
David Meir-Levy <davidmeirlevy@gmail.com>
2018-07-11 14:00:03 +0300

    router file

---
+++ src/router.ts
@@ -0,0 +1,13 @@
+import {VueRouter} from 'vue-router/types/router';
+import {Vue} from 'vue-property-decorator';
+
+
+Vue.use(VueRouter);
+
+
+export const router = new VueRouter({
+    mode: 'history',
+    routes: [
+
+    ]
+});
